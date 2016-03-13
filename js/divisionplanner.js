cd2dd3ad94f67fa3c564cc898370f6be53aa8367
Martin Groeneveld <groveld@gmail.com>
2016-03-13 23:14:18 +0100

    restructured codebase

--- js/divisionplanner.js
+++ js/divisionplanner.js
@@ -1,32 +1,23 @@
-$(document).ready(function() {
-	$.getJSON("json/weapons.json", function(data) 
-	{
-		for(var i = 0; i < data.weapons.length; i++)
-		{
-			if(i === 0)
-			{
-				$('#mainWeaponList').html('<span class="weapon-item" id="' + data.weapons[i].id + '">' + data.weapons[i].name + '</span>');
-			}
-			else
-			{
-				$('#mainWeaponList').append('<span class="weapon-item" id="' + data.weapons[i].id + '">' + data.weapons[i].name + '</span>');
-			}
-		}
+$.getJSON("json/weapons.json", function (data) {
+	$.each(data.weapons.assault, function (i, v) {
+		$('#mainWeaponList').append('<span class="weapon-item" id="' + i + '">' + v.name + '</span>');
 	});
+	$.each(data.weapons.sidearms, function (i, v) {
+		$('#sideWeaponList').append('<span class="weapon-item" id="' + i + '">' + v.name + '</span>');
+	});
+});
 
-	$('#weapons').click(function(e) {
-		console.log($(e.target).attr('id'));
-		if($(e.target).attr('id') === 'primary' || $(e.target).attr('id') === 'secondary')
-		{
-			$('#mainWeaponList').show();
-		}
-		else
-		{
-			$('#sideWeaponList').show();
-		}
-	});
+$('#weapons').click(function(e) {
+	if($(e.target).attr('id') === 'primary' || $(e.target).attr('id') === 'secondary') {
+		$('#sideWeaponList').hide();
+		$('#mainWeaponList').show();
+	}
+	else {
+		$('#mainWeaponList').hide();
+		$('#sideWeaponList').show();
+	}
+});
 
-	$('.weapon-item').click(function(e) {
-		console.log("test");
-	});
+$(".weapon-item").click(function() {
+  console.log($(this).text());
 });
